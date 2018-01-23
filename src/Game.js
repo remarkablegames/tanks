917622aae7afa06538423ab80651143f3ad4bb12
Mark <mark@remarkablemark.org>
2018-01-23 00:06:13 -0500

    feat(states): create empty state Load which starts after Boot

--- src/Game.js
+++ src/Game.js
@@ -1,11 +1,14 @@
 import Phaser from 'phaser';
-import { Boot } from './states';
+import { Boot, Load } from './states';
 
 export default class Game extends Phaser.Game {
   constructor() {
     // width, height, renderer, parent
     super(800, 600, Phaser.AUTO, document.getElementById('game'));
+
     this.state.add(Boot.key, Boot, false);
+    this.state.add(Load.key, Load, false);
+
     this.state.start(Boot.key);
   }
 }
---
+++ src/states/Load.js
@@ -0,0 +1,5 @@
+import { State } from 'phaser';
+
+export default class Load extends State {
+  static key = 'load';
+}
