5d025453bfe6c89b090feb5f38587b4433c96119
Mark <mark@remarkablemark.org>
2018-01-25 23:22:35 -0500

    feat(sprites): update Player behavior when instantiated
    Enable arcade physics, create cursor keys, and create weapon
    bullet that follows player sprite.

--- src/sprites/Player.js
+++ src/sprites/Player.js
@@ -1,5 +1,36 @@
 import { Sprite } from 'phaser';
+import { Bullet } from '../weapons';
+
+const key = 'player';
 
 export default class Player extends Sprite {
-  static key = 'player';
+  static key = key;
+
+  /**
+   * @param {Phaser.Game} game
+   * @param {Number}      x
+   * @param {Number}      y
+   */
+  constructor(game, x, y) {
+    super(game, x, y, game.cache.getBitmapData(key));
+
+    // Add sprite to the game.
+    game.add.existing(this);
+
+    // Set sprite anchor to center.
+    this.anchor.setTo(0.5);
+
+    // Enable physics for sprite.
+    game.physics.arcade.enable(this);
+
+    // The player should be bound to the world.
+    this.body.collideWorldBounds = true;
+
+    // Create cursor keys for movement.
+    this.cursors = game.input.keyboard.createCursorKeys();
+
+    // Create weapon that tracks player.
+    this.weapon = new Bullet(game);
+    this.weapon.trackSprite(this, 0, 0);
+  }
 }
