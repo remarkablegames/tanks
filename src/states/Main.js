62e84d5aeda9f964a17d5280a19ca5ea472d406e
Mark <mark@remarkablemark.org>
2018-01-25 22:30:19 -0500

    feat(states): set background and start arcade physics in Main

--- src/states/Main.js
+++ src/states/Main.js
@@ -1,5 +1,13 @@
-import { State } from 'phaser';
+import { Physics, State } from 'phaser';
 
 export default class Main extends State {
   static key = 'main';
+
+  create() {
+    // Set the background color of the stage.
+    this.stage.backgroundColor = '#121215';
+
+    // We're going to be using physics, so enable the Arcade Physics system.
+    this.physics.startSystem(Physics.ARCADE);
+  }
 }
