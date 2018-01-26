9d99e9c9256c26dd5f9e91459224441c6e06bdcb
Mark <mark@remarkablemark.org>
2018-01-25 23:39:17 -0500

    feat(sprites): add Enemy instantiation logic
    Add sprite to game, set anchor, add to group, and enable physics.

--- src/sprites/Enemy.js
+++ src/sprites/Enemy.js
@@ -1,5 +1,32 @@
 import { Sprite } from 'phaser';
+import { groups } from '../shared';
+
+const key = 'enemy';
 
 export default class Enemy extends Sprite {
-  static key = 'enemy';
+  static key = key;
+
+  /**
+   * @param {Phaser.Game} game
+   * @param {Number}      x
+   * @param {Number}      y
+   */
+  constructor(game, x, y) {
+    super(game, x, y, game.cache.getBitmapData(key));
+
+    // Add sprite to the game.
+    game.add.existing(this);
+
+    // Set sprite anchor to center.
+    this.anchor.setTo(0.5);
+
+    // Then add sprite to the group.
+    groups.enemies.add(this);
+
+    // Enable physics for sprite.
+    game.physics.arcade.enable(this);
+
+    // The player should be bound to the world.
+    this.body.collideWorldBounds = true;
+  }
 }
