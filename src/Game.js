0b48120d1a715cc130036126134ab1132d51e6ec
Mark <mark@remarkablemark.org>
2018-01-30 19:46:57 -0500

    refactor: set constructor defaults for Game class

--- src/Game.js
+++ src/Game.js
@@ -2,9 +2,14 @@ import Phaser from 'phaser';
 import { Boot, Load, Main } from './states';
 
 export default class Game extends Phaser.Game {
-  constructor() {
-    // width, height, renderer, parent
-    super(800, 600, Phaser.AUTO, document.getElementById('game'));
+  /**
+   * @param {Number|String}      [width=800]  - The game width.
+   * @param {Number|String}      [height=600] - The game height.
+   * @param {Number}             [renderer=0] - The renderer to use (AUTO, WEBGL, CANVAS, HEADLESS).
+   * @param {String|HTMLElement} [parent='']  - The parent element or id.
+   */
+  constructor(width = 800, height = 600, renderer = Phaser.AUTO, parent = '') {
+    super(width, height, renderer, parent);
 
     this.state.add(Boot.key, Boot, false);
     this.state.add(Load.key, Load, false);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import './index.css';
+import { AUTO } from 'phaser';
 
 import Game from './Game';
-new Game();
+new Game(800, 600, AUTO, document.getElementById('game'));
 
 import registerServiceWorker from './registerServiceWorker';
 registerServiceWorker();
