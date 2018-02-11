47d7078a18c6ac4cc3551d733b851fad72405cd2
Mark <mark@remarkablemark.org>
2018-02-10 22:31:30 -0500

    refactor(game): tidy Game class

--- src/Game.js
+++ src/Game.js
@@ -5,16 +5,17 @@ export default class Game extends Phaser.Game {
   /**
    * @param {Number|String}      [width=800]  - The game width.
    * @param {Number|String}      [height=600] - The game height.
-   * @param {Number}             [renderer=0] - The renderer to use (AUTO, WEBGL, CANVAS, HEADLESS).
+   * @param {Number}             [renderer=0] - The renderer to use: AUTO, WEBGL, CANVAS, HEADLESS.
    * @param {String|HTMLElement} [parent='']  - The parent element or id.
    */
   constructor(width = 800, height = 600, renderer = Phaser.AUTO, parent = '') {
     super(width, height, renderer, parent);
+    const { state } = this;
 
-    this.state.add(Boot.key, Boot, false);
-    this.state.add(Load.key, Load, false);
-    this.state.add(Main.key, Main, false);
+    state.add(Boot.key, Boot, false);
+    state.add(Load.key, Load, false);
+    state.add(Main.key, Main, false);
 
-    this.state.start(Boot.key);
+    state.start(Boot.key);
   }
 }
